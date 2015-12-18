2a97c2e7da24ca60823eeb5781cbc9193db8e28a
Albert Li <albert_lee@hotmail.com>
2015-12-18 14:32:51 -0800

    fix generate requirement for name.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 /* jshint node: true */
 'use strict';
+let path = require('path');
 
 module.exports = {
-  name: 'ember-cli-mousewheel'
+  name: 'ember-cli-mousewheel',
+  
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+
+  included: function(app) {
+    this._super.included(app);
+    this.app.import(app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.js');
+  }
 };
