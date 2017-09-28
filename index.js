1f365540e573524150d606ef6af2dac76a8bfd8b
SergeAstapov <SergeAstapov@gmail.com>
2017-09-28 12:41:31 -0400

    Upgrade ember cli (#2)
    * Move jquery.mousewheel to node modules / Ember FastBoot support
    
    * Add houndci config
    
    * Upgrade Ember CLI
    
    * Removed extraneous .hound.yml file

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 var path = require('path');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var BroccoliDebug = require('broccoli-debug');
+var fbTransform = require('fastboot-transform');
 
 module.exports = {
   name: 'ember-cli-mousewheel',
-  
+
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
   },
 
+  treeForVendor: function(vendorTree) {
+    var mousewheelDir = path.dirname(require.resolve('jquery.mousewheel'));
+
+    var mousewheelTree = new Funnel(mousewheelDir, {
+      files: ['jquery.mousewheel.js']
+    });
+    mousewheelTree = new BroccoliDebug(
+      mousewheelTree, 'ember-cli-mousewheel:mousewheel-tree'
+    );
+
+    if (vendorTree) {
+      vendorTree = mergeTrees([vendorTree, mousewheelTree]);
+    } else {
+      vendorTree = mousewheelTree;
+    }
+
+    return new BroccoliDebug(
+      fbTransform(vendorTree), 'ember-cli-mousewheel:vendor-tree'
+    );
+  },
+
   included: function(app) {
     this._super.included(app);
-    this.app.import(app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.js');
+
+    app.import({
+      development: 'vendor/jquery.mousewheel.js'
+    });
   }
 };
